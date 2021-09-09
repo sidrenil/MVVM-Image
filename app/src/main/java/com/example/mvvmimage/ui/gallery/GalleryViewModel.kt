d9762bc1de84dded8749a42d42056f88486ea2c0
sidre nil <dengesidrenil@gmail.com>
2021-09-09 15:48:04 +0300

    init

--- app/src/main/java/com/example/mvvmimage/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/com/example/mvvmimage/ui/gallery/GalleryViewModel.kt
@@ -3,16 +3,14 @@ package com.example.mvvmimage.ui.gallery
 import androidx.lifecycle.*
 import androidx.paging.cachedIn
 import com.example.mvvmimage.data.UnsplashRepository
-import dagger.assisted.Assisted
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
-
 
 @HiltViewModel
 class GalleryViewModel @Inject constructor(
     private val repository: UnsplashRepository,
 
-    state: SavedStateHandle
+     state: SavedStateHandle
 ) : ViewModel() {
 
     private val currentQuery = state.getLiveData(CURRENT_QUERY, DEFAULT_QUERY)
@@ -27,6 +25,6 @@ class GalleryViewModel @Inject constructor(
 
     companion object {
         private const val CURRENT_QUERY = "current_query"
-        private const val DEFAULT_QUERY = "cats"
+        private const val DEFAULT_QUERY = "cat"
     }
 }
